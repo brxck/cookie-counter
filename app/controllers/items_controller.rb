9317717190bc029f83ff195e7166fce613ae50a9
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-21 15:48:00 -0700

    Implement item views.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,56 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: %i[edit update show destroy]
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      flash[:success] = 'Item created.'
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'Item not created.'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update_attributes(item_params)
+      flash.now[:success] = 'Item updated.'
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'Item not updated.'
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def destroy
+    @item.destroy
+    flash[:success] = "Item deleted."
+  end
+  private
+
+  def set_item
+    if !(@item = Item.find_by(params[:id]))
+      flash[:warning] = 'Item not found.'
+      redirect_to items_path
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :unit, :category, :threshold, :value)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,28 @@
 class Item < ApplicationRecord
   has_many :entries
 
-  validates :name, :unit, :category, :value, presense: true
+  validates :name, :unit, :category, :value, presence: true
   validates :name, uniqueness: true
-  validates :threshold, :value, numericality: numericality: { only_integer: true,
-                                              greater_than_or_equal_to: 0 }
+  validates :threshold, :value, numericality: { only_integer: true,
+                                                greater_than_or_equal_to: 0 }
+
+  def in_stock
+    return unless entries.any?
+    entries.last.quantity
+  end
+
+  def on_order
+    return unless entries.any?
+    entries.last.on_order
+  end
+
+  def updated_by
+    # TODO: Add data to user model.
+    # entries.last.user.initials
+  end
+
+  def last_updated
+    return unless entries.any?
+    entries.last.updated_at
+  end
 end
