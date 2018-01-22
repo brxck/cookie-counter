639450fd159c1de83941052ed8fa0db23e6ee7af
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-22 13:43:19 -0700

    Update homepage.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     @low_stock = Item.where(low_stock: true)
     @last_update = Entry.last.created_at
+    @last_update_user = Employee.find(Entry.last.pin).name
     @entries_updated = Item.where('updated_at > ?', 1.week.ago).count
     @percent_updated = @entries_updated / Item.count * 100
     @waste = Waste.where(created_at: Date.today)
