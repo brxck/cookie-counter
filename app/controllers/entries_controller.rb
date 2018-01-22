af5752b4e1db4a217a12b432e729cc9ee9437199
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-21 22:04:35 -0700

    Start entries controls.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,25 @@
 class EntriesController < ApplicationController
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    if @entry.save
+      flash[:success] = 'Inventory updated.'
+      redirect_to entry.item
+    else
+      flash.now[:danger] = 'Inventory could not be updated.'
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:item_id, :employee_id, :quantity)
+  end
 end
