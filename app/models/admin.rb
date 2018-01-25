0be9e672ef32cb8d06570fcc82c715d7cb77721b
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-24 19:38:30 -0700

    Disable admin registration.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,8 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Re-add :registerable to register new admins.
 end
