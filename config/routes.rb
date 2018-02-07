de9b318b6e8b37000e8f528d08a215e1cd511bf6
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-02-06 22:05:20 -0700

    Refactor routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ Rails.application.routes.draw do
   resources :employees
   devise_for :admins
 
-  root 'pages#home'
-  get 'admin', to: 'pages#admin_panel', as: 'admin_panel'
-  get 'about', to: 'pages#about', as: 'about'
+  root "pages#home"
+  get "admin", to: "pages#admin_panel", as: "admin_panel"
+  get "about", to: "pages#about", as: "about"
 
   resources :items
-  get 'search', to: 'items#search'
+  get "search", to: "items#search"
 
-  get  'items/:id/update', to: 'entries#new', as: 'new_entry'
-  post 'items/:id/update', to: 'entries#create'
-  get  'update', to: 'entries#index', as: 'entries'
-  post 'update', to: 'entries#update', as: 'update_entries'
+  get  "items/:id/update", to: "entries#new", as: "new_entry"
+  post "items/:id/update", to: "entries#create"
+  get  "update", to: "entries#index", as: "entries"
+  post "update", to: "entries#update", as: "update_entries"
 
-  get  'items/:id/waste', to: 'wastes#new', as: 'new_waste'
-  post 'items/:id/waste', to: 'wastes#create'
-  get  'waste', to: 'wastes#index', as: 'wastes'
+  get  "items/:id/waste", to: "wastes#new", as: "new_waste"
+  post "items/:id/waste", to: "wastes#create"
+  get  "waste", to: "wastes#index", as: "wastes"
 end
